7cea62f9e6908da6dd5f1aa6c8a2d3a3d8d8358b
wanghveganjerky <99945874+wanghveganjerky@users.noreply.github.com>
2023-03-03 13:23:14 -0500

    hii

--- main.js
+++ main.js
@@ -3,7 +3,16 @@ const lastUpdated = new Date(document.lastModified);
 const formattedDate = `${lastUpdated.getMonth() + 1}/${lastUpdated.getDate()}/${lastUpdated.getFullYear()}`;
 lastUpdatedElem.textContent = `Last updated: ${formattedDate}`;
 
-
+// Detect device orientation
+window.addEventListener('deviceorientation', (event) => {
+    // Calculate the tilt angle
+    const tiltAngle = Math.abs(event.beta);
+  
+    // Adjust background color based on tilt angle
+    const hue = (tiltAngle / 180) * 360; // Map tilt angle to hue value
+    document.body.style.backgroundColor = `hsl(${hue}, 50%, 50%)`;
+  });
+  
 
 
 //whazzzzupppppp
