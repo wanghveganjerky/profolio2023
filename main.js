55979fddaae7d145851dbea292d09239eb738e04
wanghveganjerky <99945874+wanghveganjerky@users.noreply.github.com>
2023-03-03 02:04:03 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,4 @@
+const lastUpdatedElem = document.getElementById("last-updated");
+const lastUpdated = new Date(document.lastModified);
+const formattedDate = `${lastUpdated.getMonth() + 1}/${lastUpdated.getDate()}/${lastUpdated.getFullYear()}`;
+lastUpdatedElem.textContent = `Last updated: ${formattedDate}`;
