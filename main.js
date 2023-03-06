4e00f96bd1a8715f16cac941621b0e60108d6589
wanghveganjerky <99945874+wanghveganjerky@users.noreply.github.com>
2023-03-06 01:48:51 -0500

    111

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-const updatedDateElem = document.getElementById("last-updated");
-const options = { timeZone: "America/New_York" };
+// const updatedDateElem = document.getElementById("last-updated");
+// const options = { timeZone: "America/New_York" };
 
-function updateTime() {
-  const now = new Date();
-  const formattedDate = now.toLocaleString("en-US", options);
-  updatedDateElem.textContent = `${formattedDate}`;
-}
+// function updateTime() {
+//   const now = new Date();
+//   const formattedDate = now.toLocaleString("en-US", options);
+//   updatedDateElem.textContent = `${formattedDate}`;
+// }
 
-updateTime();
-setInterval(updateTime, 1000);
+// updateTime();
+// setInterval(updateTime, 1000);
 
 
 //whazzzzupppppp
