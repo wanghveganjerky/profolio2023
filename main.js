5839ede3219f6b64eec9ae3398f9f7dc45933dee
haotianw177 <wangh124@newschool.edu>
2023-06-20 15:55:30 -0400

    111

--- main.js
+++ main.js
@@ -1,21 +1,2 @@
-// const updatedDateElem = document.getElementById("last-updated")
-// const options = { timeZone: "America/New_York", hour: "numeric", minute: "numeric", }
-
-// function updateTime() {
-//   const now = new Date()
-//   const formattedDate = now.toLocaleString("en-US", options)
-//   updatedDateElem.textContent = `${formattedDate}`
-// }
-
-// updateTime()
-// document.getElementById('wobble-element').addEventListener('mouseover', function(){
-//     this.classList.add('wobble');
-//   });
-  
-//   document.getElementById('wobble-element').addEventListener('animationend', function(){
-//     this.classList.remove('wobble');
-//   });
-  
-
 
 //whazzzzupppppp
