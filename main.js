246b5053cc5da9d838ca72465111e2abcbcd5ea1
wanghveganjerky <99945874+wanghveganjerky@users.noreply.github.com>
2023-09-23 17:19:16 -0400

    2

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 // Fetch all h2 elements within li tags
 const h2Elements = document.querySelectorAll('li h2');
-
 // Fetch the preview div and image
 const hoverPreviewDIV = document.getElementById('h2-hover-image-preview-div');
 const hoverPreviewIMG = document.getElementById('h2-hover-image-preview-img');
@@ -14,21 +13,16 @@ function showHoverPreview(event) {
     targetElement = targetElement.parentElement;
     if (!targetElement) return; // Exit if we've reached the root and found no h2
   }
-
+  
   // Get the image source from the data attribute
   const imgFileName = targetElement.getAttribute('data-img-src');
-
   // Set the image source
   hoverPreviewIMG.src = `images/${imgFileName}`;
-
-  // Position the preview div next to the cursor
   hoverPreviewDIV.style.left = (event.pageX + 10) + 'px';
   hoverPreviewDIV.style.top = (event.pageY + 10) + 'px';
-
   // Show the preview div
   hoverPreviewDIV.style.display = 'block';
 }
-
 
 // Function to hide preview
 function hideHoverPreview() {
