00fd09f2a7ec55e470143623a00dc5331b22377d
wanghveganjerky <99945874+wanghveganjerky@users.noreply.github.com>
2023-11-04 17:02:13 -0400

    1

--- main.js
+++ main.js
@@ -1,40 +1,3 @@
-// Fetch all h2 elements within li tags
-const h2Elements = document.querySelectorAll('li h2');
-// Fetch the preview div and image
-const hoverPreviewDIV = document.getElementById('h2-hover-image-preview-div');
-const hoverPreviewIMG = document.getElementById('h2-hover-image-preview-img');
-
-// Function to show hover preview
-function showHoverPreview(event) {
-  let targetElement = event.target;
-
-  if (targetElement.tagName.toLowerCase() !== 'h2') {
-    return;
-  }
-
-  // Get the image source from the data attribute
-  const imgFileName = targetElement.getAttribute('data-img-src');
-  // Set the image source
-  hoverPreviewIMG.src = `images/${imgFileName}`;
-  hoverPreviewDIV.style.left = (event.pageX + 10) + 'px';
-  hoverPreviewDIV.style.top = (event.pageY + 10) + 'px';
-  // Show the preview div
-  hoverPreviewDIV.style.display = 'block';
-}
-
-// Function to hide preview
-function hideHoverPreview() {
-  hoverPreviewDIV.style.display = 'none';
-}
-
-// Attach event listeners
-h2Elements.forEach(h2 => {
-  h2.addEventListener('mouseover', showHoverPreview);
-  h2.addEventListener('mousemove', showHoverPreview);
-  h2.addEventListener('mouseout', hideHoverPreview);
-});
-
-// Fetch last updated date from GitHub API
 function fetchLastUpdated() {
   const repoOwner = 'wanghveganjerky'; 
   const repoName = 'profolio2023';  
@@ -87,6 +50,42 @@ document.addEventListener('DOMContentLoaded', function() {
     linkOfTheWeek.href = 'https://www.sourcetype.com/editorial/14248/ground-writing'; 
   }
 });
+
+// Fetch all h2 elements within li tags
+// const h2Elements = document.querySelectorAll('li h2');
+// // Fetch the preview div and image
+// const hoverPreviewDIV = document.getElementById('h2-hover-image-preview-div');
+// const hoverPreviewIMG = document.getElementById('h2-hover-image-preview-img');
+
+// Function to show hover preview
+// function showHoverPreview(event) {
+//   let targetElement = event.target;
+
+//   if (targetElement.tagName.toLowerCase() !== 'h2') {
+//     return;
+//   }
+
+  // Get the image source from the data attribute
+  // const imgFileName = targetElement.getAttribute('data-img-src');
+  // Set the image source
+  // hoverPreviewIMG.src = `images/${imgFileName}`;
+  // hoverPreviewDIV.style.left = (event.pageX + 10) + 'px';
+  // hoverPreviewDIV.style.top = (event.pageY + 10) + 'px';
+  // Show the preview div
+  // hoverPreviewDIV.style.display = 'block';
+// }
+
+// Function to hide preview
+// function hideHoverPreview() {
+//   hoverPreviewDIV.style.display = 'none';
+// }
+
+// // Attach event listeners
+// h2Elements.forEach(h2 => {
+//   h2.addEventListener('mouseover', showHoverPreview);
+//   h2.addEventListener('mousemove', showHoverPreview);
+//   h2.addEventListener('mouseout', hideHoverPreview);
+// });
 
 
 
